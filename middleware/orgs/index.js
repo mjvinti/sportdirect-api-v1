3df34c28a63c119063d44e20135620f509ad3759
Mario Vinti <mjvinti@gmail.com>
2023-01-06 12:35:29 -0500

    configured testing setup, wrote tests for loadOrg middleware

--- middleware/orgs/index.js
+++ middleware/orgs/index.js
@@ -1,24 +1,26 @@
 const db = require("../../lib/db");
 
 exports.loadOrg = async (req, res, next) => {
-  const { orgId = null } = req.params;
-  const OrgModel = db.loadModel("org");
+  const {
+    params: { orgId = null },
+  } = req;
 
   if (!orgId) {
     return res
       .status(400)
-      .json("You provide the following required parameters: orgId");
+      .json("You must provide the following required parameters: orgId");
   }
 
   try {
-    const foundOrg = await OrgModel.findByPk(orgId);
+    const foundOrg = await db.loadModel("org").findByPk(orgId);
     if (!foundOrg) {
       return res.status(404).json(`There is no org for id: ${orgId}`);
     }
     req.org = foundOrg;
     next();
   } catch (err) {
-    console.log(err);
-    return res.json("Something went wrong");
+    return res
+      .status(500)
+      .json("Something went wrong loading the org. Please try again later.");
   }
 };
